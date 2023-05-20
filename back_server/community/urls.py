61050d38ab054562c3e55d041cb1224d82d6e6bc
dang2 <dndeo10814@gmail.com>
2023-05-21 00:05:21 +0900

    버전 맞추기

--- back_server/community/urls.py
+++ back_server/community/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "community"
 urlpatterns = [
+    path('editor_articles/', views.editor_articles),
+    path('editor_articles/<int:editor_article_pk>/', views.editor_article_detail),
+    path('user_articles/', views.user_articles),
+    path('user_articles/<int:user_article_pk>', views.user_article_detail)
 ]
