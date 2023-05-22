7f8e14bdfa13fe68e84959199336fa97ae5db960
sojeong <thwjd025@gmail.com>
2023-05-22 14:01:13 +0900

    팔로우 완

--- front_server/src/views/accounts/ProfileView.vue
+++ front_server/src/views/accounts/ProfileView.vue
@@ -4,12 +4,19 @@
     <div class="profile-left">
       <p>{{user.username}}님의 프로필</p>
       <p>{{user.profile_img}} 지금은 이미지 없음</p>
-      <div v-if="isFollow">
-        <button @click="follow" class="follow">언팔로우</button>
+
+      <div v-if="itsMe">
+        <span class="itsme">안보이게하기</span>
       </div>
       <div v-else>
-      <button @click="follow" class="follow">팔로우</button>
+        <div v-if="isFollow">
+          <span @click="follow" class="follow">언팔로우</span>
+        </div>
+        <div v-else>
+          <span @click="follow" class="follow">팔로우</span>
+        </div>
       </div>
+
       <span class="follower-number">팔로워 : {{ followings.length }}</span>
       <span class="following-number">팔로잉 : {{ followers.length }}</span>
     </div>
@@ -49,17 +56,28 @@ export default {
       followers: [],
       followings: [],
       isFollow: false,
+      itsMe: null,
     }
   },
   methods: {
     getUserProfile(){
       const user_id = jwtDecode(token).user_id
-      const userId = this.$route.params.id
-      console.log(user_id)
-      console.log(userId)
+      const profile_id = this.$route.params.id
+
+      const checkId = parseInt(profile_id)
+
+      if (user_id === checkId) {
+        this.itsMe = true
+        console.log('본인 페이지임')
+        console.log(this.itsMe)
+      } else {
+        this.itsMe = false
+        console.log('다른 사람 페이지임')
+      }
+
       axios({
         method: 'get',
-        url: `${API_URL}/accounts/profile/${userId}/`,
+        url: `${API_URL}/accounts/profile/${profile_id}/`,
         headers: {
           Authorization: `JWT ${token}`
         }
@@ -138,12 +156,23 @@ export default {
   border: 2px solid fuchsia;
   margin: 10px;
 }
+.follow{
+  margin: 10px;
+  cursor: pointer;
+  background-color: black;
+  color: white
+}
 .follower-number {
   border: 2px solid blueviolet;
   border-radius: 20px;
+  margin: 20px;
 }
 .following-number {
   border: 2px solid blueviolet;
   border-radius: 20px;
+  margin: 20px;
+}
+.itsme{
+  visibility: hidden;
 }
 </style>
