2cac1b8639a7ad82daa44dd9b8a5c3bca036bb27
sojeong <thwjd025@gmail.com>
2023-05-23 17:49:56 +0900

    user-article-detail

--- front_server/src/main.js
+++ front_server/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(fas)
 
 Vue.config.productionTip = false
 
